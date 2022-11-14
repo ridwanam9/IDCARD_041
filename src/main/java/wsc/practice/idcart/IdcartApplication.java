269b16508df30774beeb6be457c9f271973b7d5f
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-11-14 22:25:14 +0700

    first commit

---
+++ src/main/java/wsc/practice/idcart/IdcartApplication.java
@@ -0,0 +1,13 @@
+package wsc.practice.idcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsc/practice/idcart/IdcartApplicationTests.java
@@ -0,0 +1,13 @@
+package wsc.practice.idcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
