44f0c9e500c1af280c993c9ceabb1018eed81a55
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-11-14 23:55:02 +0700

    menambah requestparam pada controller

--- src/main/java/wsc/practice/idcart/IdCartController.java
+++ src/main/java/wsc/practice/idcart/IdCartController.java
@@ -4,9 +4,16 @@
  */
 package wsc.practice.idcart;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -16,13 +23,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IdCartController {
     
     @ResponseBody
-    @RequestMapping("/getData")
-    public String getdata(){
+    @RequestMapping("/mimosa")
+    public String getdata(@RequestParam("nama")String text,
+            @RequestParam("image")MultipartFile image, 
+            @RequestParam("tanggal") @DateTimeFormat(pattern="yyyy-MM-dd") Date date)
+            throws IOException{
         
-        String text = "Hello World";
+        SimpleDateFormat tanggal = new SimpleDateFormat("EE-dd-MMMM-yyyy");
+        String newTanggal = tanggal.format(date);
+        String blob = Base64.encodeBase64String(image.getBytes());
+        
+        return text +"<br>" + newTanggal+ "<br> <img width=100' src='data:image/jpeg;base64, "+
+                blob+" '><br>";
     
-    
-        return text;
     }
     
 }
