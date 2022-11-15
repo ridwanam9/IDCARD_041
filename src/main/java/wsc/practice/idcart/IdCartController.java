532ec30624cefe60099920a32f55d39a93768d52
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-11-15 15:45:47 +0700

    menambahkan template untuk hasil output

--- src/main/java/wsc/practice/idcart/IdCartController.java
+++ src/main/java/wsc/practice/idcart/IdCartController.java
@@ -7,9 +7,11 @@ package wsc.practice.idcart;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,9 +24,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class IdCartController {
     
-    @ResponseBody
+    //@ResponseBody
     @RequestMapping("/mimosa")
-    public String getdata(@RequestParam("nama")String text,
+    public String getdata(Model model,
+            @RequestParam("nama")String text,
             @RequestParam("image")MultipartFile image, 
             @RequestParam("tanggal") @DateTimeFormat(pattern="yyyy-MM-dd") Date date)
             throws IOException{
@@ -32,9 +35,21 @@ public class IdCartController {
         SimpleDateFormat tanggal = new SimpleDateFormat("EE-dd-MMMM-yyyy");
         String newTanggal = tanggal.format(date);
         String blob = Base64.encodeBase64String(image.getBytes());
+        String img = "data:image/*;base64,".concat(blob);
         
-        return text +"<br>" + newTanggal+ "<br> <img width=100' src='data:image/jpeg;base64, "+
-                blob+" '><br>";
+        model.addAttribute("nama", text);
+        model.addAttribute("ttl", newTanggal);
+        model.addAttribute("gambar", img);
+        
+        
+        
+        
+        
+        
+//        return "Name : "+ text +"<br>" + "Born : "+newTanggal+ "<br> <img width=100' src='data:image/jpeg;base64, "+
+//                blob+" '><br>";
+
+        return "viewcart";
     
     }
     
